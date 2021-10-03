0542db60227fd972655e122f2e0e5164c8b34488
pendimadyana <pendimadyana@gmail.com>
2021-10-03 20:54:06 +0700

    copy css from brad repository to index.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 const Header = ({ title }) => {
   return (
     <header>
-      <h1 style={headingStyle}>{title}</h1>
+      <h1>{title}</h1>
     </header>
   );
 };
@@ -14,11 +14,6 @@ Header.defaultProps = {
 
 Header.propTypes = {
   title: PropTypes.string.isRequired,
-};
-
-const headingStyle = {
-  color: "red",
-  backgroundColor: "yellow",
-};
+}
 
 export default Header;

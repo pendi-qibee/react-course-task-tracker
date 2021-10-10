334e46b2c0554a1030b5df5cdbe65a4f8dd6ad44
pendimadyana <pendimadyana@gmail.com>
2021-10-10 08:46:57 +0700

    useState Hook intro

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,36 +1,38 @@
-const tasks = [
-    {
-        id: 1,
-        text: 'Bangun Tidur',
-        day: 'Senin, 8 Okt 2021',
-        reminder: true,
-    },
-
-    {
-        id: 2,
-        text: 'Olahraga',
-        day: 'Senin, 8 Okt 2021',
-        reminder: true,
-    },
-
-    {
-        id: 3,
-        text: 'Sarapan',
-        day: 'Senin, 8 Okt 2021',
-        reminder: true,
-    },
-
-    {
-        id: 4,
-        text: 'Bersih-bersih',
-        day: 'Senin, 8 Okt 2021',
-        reminder: false,
-    },
-]
-
+import { useState } from 'react'
 
 const Tasks = () => {
+    const [tasks, setTasks] = useState([
+        {
+            id: 1,
+            text: 'Bangun Tidur',
+            day: 'Senin, 8 Okt 2021',
+            reminder: true,
+        },
+
+        {
+            id: 2,
+            text: 'Olahraga',
+            day: 'Senin, 8 Okt 2021',
+            reminder: true,
+        },
+
+        {
+            id: 3,
+            text: 'Sarapan',
+            day: 'Senin, 8 Okt 2021',
+            reminder: true,
+        },
+
+        {
+            id: 4,
+            text: 'Bersih-bersih',
+            day: 'Senin, 8 Okt 2021',
+            reminder: false,
+        },
+    ])
+
     return (
+        setTasks([...tasks, {}])
         <>
             {tasks.map((task) => (
                 <h3 key={task.id}>{task.text}</h3>

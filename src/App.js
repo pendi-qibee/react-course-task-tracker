2cedcb4627c0f175719742f9e66eab1eca02af49
pendimadyana <pendimadyana@gmail.com>
2021-10-10 08:52:32 +0700

    add list to global state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import { useState } from 'react'
 
 const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Bangun Tidur',
+      day: 'Senin, 8 Okt 2021',
+      reminder: true,
+    },
+
+    {
+      id: 2,
+      text: 'Olahraga',
+      day: 'Senin, 8 Okt 2021',
+      reminder: true,
+    },
+
+    {
+      id: 3,
+      text: 'Sarapan',
+      day: 'Senin, 8 Okt 2021',
+      reminder: true,
+    },
+
+    {
+      id: 4,
+      text: 'Bersih-bersih',
+      day: 'Senin, 8 Okt 2021',
+      reminder: false,
+    },
+  ])
+
   return (
     <div className='container'>
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks} />
     </div>
   )
 }

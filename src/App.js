e3dde307f52cf5d4343ff7f34ea3b67deee9765b
pendimadyana <pendimadyana@gmail.com>
2021-10-10 16:28:23 +0700

    add task sumbit functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
+import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
-import { useState } from 'react'
+import AddTask from './components/AddTask'
 
 const App = () => {
   const [tasks, setTasks] = useState([
@@ -43,9 +44,21 @@ const App = () => {
     setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
   }
 
+  // add task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+    console.log(task)
+    console.log(id)
+
+  }
+
+
   return (
     <div className='container'>
       <Header />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No task to show'}
     </div>
   )

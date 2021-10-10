ca784271f220dd5b0b0d995d540c620590399c72
pendimadyana <pendimadyana@gmail.com>
2021-10-10 10:19:48 +0700

    delete task item and remove it from browser

--- src/App.js
+++ src/App.js
@@ -35,14 +35,14 @@ const App = () => {
 
   //delete task
   const deleteTask = (id) => {
-    console.log('delete', id);
+    setTasks(tasks.filter((task) => task.id !== id))
   }
 
 
   return (
     <div className='container'>
       <Header />
-      <Tasks tasks={tasks} onDelete={deleteTask}/>
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : 'No task to show'}
     </div>
   )
 }

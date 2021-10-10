a30928fcf0b4323026a43318ee8c59e121dc4e0f
pendimadyana <pendimadyana@gmail.com>
2021-10-10 16:56:14 +0700

    add header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import Button from './Button'
 const Header = ({ title, onAdd, showAdd }) => {
 
     return (
-        <header>
+        <header className='header'>
             <h1>{title}</h1>
             <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd}></Button>
         </header>

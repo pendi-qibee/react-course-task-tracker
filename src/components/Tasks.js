495158566390c30b66fb139c7a77bfd6f885d36f
pendimadyana <pendimadyana@gmail.com>
2021-10-10 08:16:43 +0700

    show task list

---
+++ src/components/Tasks.js
@@ -0,0 +1,42 @@
+const tasks = [
+    {
+        id: 1,
+        text: 'Bangun Tidur',
+        day: 'Senin, 8 Okt 2021',
+        reminder: true,
+    },
+
+    {
+        id: 2,
+        text: 'Olahraga',
+        day: 'Senin, 8 Okt 2021',
+        reminder: true,
+    },
+
+    {
+        id: 3,
+        text: 'Sarapan',
+        day: 'Senin, 8 Okt 2021',
+        reminder: true,
+    },
+
+    {
+        id: 4,
+        text: 'Bersih-bersih',
+        day: 'Senin, 8 Okt 2021',
+        reminder: false,
+    },
+]
+
+
+const Tasks = () => {
+    return (
+        <>
+            {tasks.map((task) => (
+                <h3>{task.text}</h3>
+            ))}
+        </>
+    )
+}
+
+export default Tasks
